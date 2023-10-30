f9f6ce96b5a4ef5428d2f5c93bac8563d2c92e77
klmit <klm@smtw.in>
2023-10-30 20:21:14 +0530

    RESISTANCE

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import time
-def heat_up(ts,t0,t):
-    rate=(t0-ts)/t
+def temp_disp(ts,t0,t,rate):
     print('rate is ',rate)
-    T_vs_t=[]
-    for i in range(t):
-        time.sleep(2)
+    T_vs_t= []
+    for i in range(t+1):
         x=ts+i*rate
         print(x)
         T_vs_t.append(x)
     print(T_vs_t)
 
+def heat_coil(R,V):
+    return V**2/R
 
-T_hot=100
-T_room=50
+
+T_hot=50
+T_room=100
 t_min=20
-print('2 sec is one min')
-heat_up(T_hot,T_room,t_min)
+
+rate=(T_room-T_hot)/t_min
+temp_disp(T_hot,T_room,t_min,rate)
