115b035d30bdc97a3aae82099fcf891163451161
klmit <klm@smtw.in>
2023-11-03 00:11:16 +0530

    resistivity ,setup data

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-def temp_disp(ts,t0,t,rate):
-    print('rate is ',rate)
-    T_vs_t= []
+def temp_disp(ts, t0, t, rate):
+    print('rate is ', rate)
+    T_vs_t = []
     for i in range(t+1):
-        x=ts+i*rate
+        x = ts+i*rate
         print(x)
         T_vs_t.append(x)
     print(T_vs_t)
 
 def heat_coil(R,V):
+    #P=V^2/R
     return V**2/R
 
+def v_resistivity(r0,a,t):
+    #rt=r0(1+at)
+    return r0*(1+a*t)
 
-T_hot=50
-T_room=100
-t_min=20
+def resistance():
 
-rate=(T_room-T_hot)/t_min
-temp_disp(T_hot,T_room,t_min,rate)
+    return
+
+
+T_hot = 50
+T_room = 100
+t_min = 20
+rate = (T_room-T_hot)/t_min
+temp_disp(T_hot, T_room, t_min, rate)
--- rate.py
+++ rate.py
@@ -1,5 +1,10 @@
 import math
+
+
 def cooling(Ts,To,k,t):
     #T(t) = Ts + (Ts – To) e^(-kt)
     #for the time being just returns final temp
-    return Ts+(Ts-To)*(math.e)**(-1*k*t)
+    return Ts+(Ts-To)*math.e**(-1*k*t)
+
+
+
