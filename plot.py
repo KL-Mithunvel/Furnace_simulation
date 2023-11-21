7d23074a7393f1ac1f5384672858e3d7e0534b9d
KLA <smtwkla@gmail.com>
2023-11-21 22:15:56 +0530

    separated plot.py

---
+++ plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import pandas as ps
+
+def prepare_data(env):
+    """
+    Prepares data from the timeline for plotting. Returns Pandas DataFrame with variables needed for graph
+    :param env: pass the environment dictionary
+    :return: Pandas DataFrame
+    """
+    req = [x for x in env['var'].keys() if x != 'time' and env['var'][x]['graph']]
+    time = get_var_data_series('time')
+    data = dict()
+    for i in req:
+        data[i] = get_var_data_series(i)
+    df = ps.DataFrame(data, index=time)
+
+    return df
+
+
+def get_var_data_series(key):
+
+    data = ps.Series([x[key] for x in e["tl"]])
+    return data
+
+
+def display_graph(data_f):
+    ax = data_f.plot()
+    ax.set_ylabel('Temperature °C')
+    ax.set_xlabel('time')
+    ax.set_title('Simulation Result')
+    ax.grid(True)
+    plt.show()
--- sim_env.py
+++ sim_env.py
@@ -139,35 +139,7 @@ def run_sim(env):
         prev_ti = ti
 
 
-def prepare_data(env):
-    """
-    Prepares data from the timeline for plotting. Returns Pandas DataFrame with variables needed for graph
-    :param env: pass the environment dictionary
-    :return: Pandas DataFrame
-    """
-    req = [x for x in env['var'].keys() if x != 'time' and env['var'][x]['graph']]
-    time = get_var_data_series('time')
-    data = dict()
-    for i in req:
-        data[i] = get_var_data_series(i)
-    df = ps.DataFrame(data, index=time)
 
-    return df
-
-
-def get_var_data_series(key):
-
-    data = ps.Series([x[key] for x in e["tl"]])
-    return data
-
-
-def display_graph(data_f):
-    ax = data_f.plot()
-    ax.set_ylabel('Temperature °C')
-    ax.set_xlabel('time')
-    ax.set_title('Simulation Result')
-    ax.grid(True)
-    plt.show()
 
 
 e = init_env()
