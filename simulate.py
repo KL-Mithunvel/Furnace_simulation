83390ffcb65eed9fbc9230457fd23f54025f2ae6
KLA <smtwkla@gmail.com>
2023-11-29 22:06:42 +0530

    q & d pseudo code

--- simulate.py
+++ simulate.py
@@ -15,7 +15,15 @@ def run_sim(env):
         # 1. calculate cooling of furnace
         # 2. required temp difference = target temp - cooled furnace temp
         # 3. calculate fuel qty required to produce the temp diff required
+        fuel_required_kg = joules_required / fuel_calorific value
         # 4. clamp fuel qty with max & if required, recalculate temp diff achieved
+        if fuel_required_kg > max_allowed_fuel:
+            fuel_required_kg = max_allowed_fuel
+            joules_achieved = fuel_required_kg * fuel_calorific_value
+        else:
+            joules_achieved = joules_required
+            temp_diff_achieved = joules_achieved / material_specific_heat
+
         # 5. save calculation in TL
         r = (100 - (random.randrange(-10, 10)))/100
         ti['furnace_temp'] = ti['f_target_temp'] * r
